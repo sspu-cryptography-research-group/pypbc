f2248378deac771f4f5dae87a34e9ae377d31e94
Null Lin <37645035+nulllinhao@users.noreply.github.com>
2024-08-23 22:22:19 +0800

    Add automatically installation script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Installing pypbc..." &&
+sudo apt update &&
+sudo apt-get install -y python3-pip &&
+sudo pip3 install virtualenv virtualenvwrapper &&
+virtualenv cryptoenv &&
+source cryptoenv/bin/activate &&
+sudo apt-get install -y libgmp-dev && 
+sudo apt-get install -y build-essential flex bison &&
+wget https://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz &&
+tar -xf pbc-0.5.14.tar.gz &&
+cd pbc-0.5.14 &&
+./configure --prefix=/usr --enable-shared &&
+make &&
+sudo make install &&
+sudo ldconfig &&
+cd .. &&
+sudo ./cryptoenv/bin/pip3 install ./pypbc &&
+echo "pypbc installed successfully!"
